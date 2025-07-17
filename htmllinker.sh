a65cd89231060b7afecb52901cfe7d3630ce641c
dpentland <dpentland.grant@gmail.com>
2025-07-17 14:06:55 -0400

    Overhaul of website

---
+++ htmllinker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+outfile="index.md"
+echo > "$outfile"
+#find "." -mindepth 1 -maxdepth 1 -type d ! -name ".git"|while read folder; do #doesn't order the folders
+while read folder  ; do
+    echo "$folder"
+    echo "# ${folder##*/}" >> "$outfile"
+    find "$folder" -name "*.html" -maxdepth 3 | sort -t '\0' -n | awk -F '\0' '{print $0}' | while read file; do
+        relpath="${file#*/}"
+        linkname="${relpath#*/}"
+        linkname="${linkname%/*}"
+        #echo "$file"
+        #echo "$relpath"
+        #echo "$linkname"
+        pdflink="${file/.\//https://github.com/dpentland/dpentland.github.io/tree/master/files}"
+        #pdflink=$(sed 's/ /%20/g' <<< "$pdflink")
+        #pdflink=$(sed 's/_/%20/g' <<< "$pdflink")
+        #pdflink=$(sed 's/.html/.pdf/g' <<< "$pdflink")
+        change="\/${pdflink##*/}"
+        pdflink=$(sed "s/$change/.pdf/g" <<< "$pdflink")
+        #echo "$pdflink"
+        echo >> "$outfile"
+        #echo $change
+        echo "[$linkname]($relpath) [(pdf)]($pdflink)" >> "$outfile"
+    done
+done < folderorder.txt
